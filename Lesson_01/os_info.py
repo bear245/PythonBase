65d6fa4b02017854d3bc69c1693bcb01fe0df66a
Oleksandr Siora <alexandrsiora@gmail.com>
2021-02-10 19:25:04 +0200

    - Add new and translate existing comments to English

--- Lesson_01/os_info.py
+++ Lesson_01/os_info.py
@@ -1,11 +1,13 @@
-# Нужно собрать информацию об операционной системе и версии питона
-
+# Have to collect Work Stations information
 import platform
 import sys
 
 info = 'OS info is \n{} \n\nPython version is {}'.format(
     platform.uname(), sys.version, platform.architecture())
+
+# Show "info" on the display
 print(info)
 
+# Open external txt file and write "info" into it. Close will perform automatically
 with open('os_info.txt', 'w') as ff:
     ff.write(info)
