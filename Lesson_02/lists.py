b07238584acd0f2487f04aff78a42d1aba7a2447
Oleksandr Siora <alexandrsiora@gmail.com>
2021-02-13 15:29:56 +0200

    - few examples of using methods of lists

---
+++ Lesson_02/lists.py
@@ -0,0 +1,24 @@
+print('Hello world', 2**10)
+#user_input = input('Enter two numbers')
+
+x, y = map(int, input().split())
+print(type(x))
+print(x, y)
+
+# Length of string - len()
+# Search in string - .find(), rfind() - search from left/right
+# Find and Replace char in string - .replace() - two parameters
+# Change case - .lower(), .upper()
+# Test string .isalpha(), .isdigit()
+
+my_list = [0, 1, 2, 'string', 4]
+list(range(5, 10, 2))
+# .append() - add element to the end of list
+# .extend() - insert new list to the end
+# .insert() - insert element into concrete place of list
+# .del my_list[5] - delete 5th element
+# my_list.remove(5) - del current element from the list
+# 5 in my_list - check existance of 5 in the list
+# my_list.count(5) - count a quantity of elements in the list
+# min(my_list) / max(my_list)
+# my_list.sort()
