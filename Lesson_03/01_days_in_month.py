6c438617f5a5c5a3ee8c65fa1c3103924773c5fd
Oleksandr <alexandrsiora@gmail.com>
2021-02-17 15:56:09 +0200

    - HA done by list and dictionary

--- Lesson_03/01_days_in_month.py
+++ Lesson_03/01_days_in_month.py
@@ -8,7 +8,15 @@
 
 # Номер месяца получать от пользователя следующим образом
 user_input = input("Введите, пожалуйста, номер месяца: ")
-month = int(user_input)
-print('Вы ввели', month)
+month = int(user_input) - 1
 
-# TODO здесь ваш код
+keys_list = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December']
+year_dict = {'January':30, 'February':28, 'March':31, 'April':30, 'May':31, 'June':30,
+        'July':31, 'August':30, 'September':31, 'October':30, 'November':31, 'December':32}
+
+if -1 < month < 12:
+    print('Вы ввели ' + str(keys_list[month]) + '. Количество дней = ' + str(year_dict.get(keys_list[month])))
+else:
+    print('Вы ввели некорректный номер месяца', month + 1)
+
