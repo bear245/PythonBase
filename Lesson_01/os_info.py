ff39d4f8ea6b853e0d7d7f9ca156aad7e7e1dda4
Oleksandr <alexandrsiora@gmail.com>
2021-02-19 10:21:25 +0200

    - Done 00 home assignments of lesson_03

--- Lesson_01/os_info.py
+++ Lesson_01/os_info.py
@@ -9,5 +9,5 @@ info = 'OS info is \n{} \n\nPython version is {}'.format(
 print(info)
 
 # Open external txt file and write "info" into it. Close will perform automatically
-with open('os_info.txt', 'w') as ff:
-    ff.write(info)
+# with open('os_info.txt', 'w') as ff:
+#    ff.write(info)
