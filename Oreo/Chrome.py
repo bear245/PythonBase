df7b6b7b3d1f4909536b37888157ec4672f1cb80
Oleksandr Siora <alexandrsiora@gmail.com>
2021-03-06 19:51:51 +0200

    - Add debug strings for every action in the loop

--- Oreo/Chrome.py
+++ Oreo/Chrome.py
@@ -1,7 +1,7 @@
 import time
 import pyautogui
 
-width, height = pyautogui.size()#returns a screen resolution
+width, height = pyautogui.size()    # returns a screen resolution
 print(str(width) + ' ' + str(height))
 
 codesFile = open('C:\\Users\\Oleksandr Siora\\PycharmProjects\\PythonBase\\Oreo\\codes.txt') #Open file in ReadMode
@@ -15,20 +15,23 @@ for i in listCodes:
     EMAIL = CODE.lower() + '@gmail.com'
 
     # Enter the CODE
-    time.sleep(5)  # Pause 2 seconds
+    print('1.Enter CODE...' + CODE)
+    time.sleep(5)  # Pause 5 seconds
     pyautogui.moveTo(2655, 500)
     pyautogui.click()
     pyautogui.typewrite(CODE, interval=0.1)
 
     #Enter the EMAIL
-    time.sleep(5)  # Pause 2 seconds
+    print('2.Enter EMAIL...')
+    time.sleep(5)  # Pause 5 seconds
     #pyautogui.press('tab')
     pyautogui.moveTo(2655, 585)
     pyautogui.click()
     pyautogui.typewrite(EMAIL, interval=0.1)
 
-    time.sleep(5)  # Pause 2 seconds
     # Set CheckBox
+    print('3.Set Checkbox...')
+    time.sleep(5)  # Pause 5 seconds
     pyautogui.moveTo(2645, 665)
     pyautogui.click()
 
@@ -38,18 +41,21 @@ for i in listCodes:
     pyautogui.click()
 
     # Check Result
+    print('4.Close Advertisement...')
     time.sleep(5) # Pause 2 seconds
-    pyautogui.moveTo(2810, 945)
+    pyautogui.moveTo(2810, 945) # Need to check and correct coordinates
     pyautogui.click()
-    time.sleep(30)
 
     # Locate result button on the screen
+    print('5.Try to find TryAgain button...')
+    time.sleep(30)
     Result = pyautogui.locateOnScreen('tryagain.png', confidence=0.8)
     print(Result)
     if Result != None:
         break;
 
     # Backward to previous page and refresh
+    print('6.Chrome back & refresh page...')
     pyautogui.hotkey('alt', 'left')
     time.sleep(10)
     pyautogui.press('f5')
